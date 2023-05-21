ccb3821119f0f32b51dbb795b0de4b8bdaac4b11
Макаров Игорь (Makarov_IV) <Makarov_IV@directum.ru>
2023-05-21 17:30:29 +0400

    feat: добавлена поддержка фильтрации строк с использованием регулярных выражений

--- LogViewer/MainWindow.xaml.cs
+++ LogViewer/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using LogReader;
+using LogReader;
 using Microsoft.Toolkit.Uwp.Notifications;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
@@ -16,6 +16,7 @@ using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
+using System.Text.RegularExpressions;
 
 namespace LogViewer
 {
@@ -498,12 +499,24 @@ namespace LogViewer
     {
       var result = true;
 
+
       if (!string.IsNullOrEmpty(text))
       {
-        result = (!string.IsNullOrEmpty(line.FullMessage) && line.FullMessage.IndexOf(text, StringComparison.OrdinalIgnoreCase) > -1) ||
-          (!string.IsNullOrEmpty(line.Trace) && line.Trace.IndexOf(text, StringComparison.OrdinalIgnoreCase) > -1) ||
-          (!string.IsNullOrEmpty(line.Pid) && line.Pid.IndexOf(text, StringComparison.OrdinalIgnoreCase) > -1) ||
-          (!string.IsNullOrEmpty(line.Level) && line.Level.IndexOf(text, StringComparison.OrdinalIgnoreCase) > -1);
+        try
+        {
+          Regex regex = new Regex(text, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+          if (this.UseRegex.IsChecked.Value)
+            result = !string.IsNullOrEmpty(line.FullMessage) && regex.IsMatch(line.FullMessage);
+          else
+            result = !string.IsNullOrEmpty(line.FullMessage) && line.FullMessage.IndexOf(text, StringComparison.OrdinalIgnoreCase) > -1;
+          result = result || (!string.IsNullOrEmpty(line.Trace) && line.Trace.IndexOf(text, StringComparison.OrdinalIgnoreCase) > -1) ||
+                             (!string.IsNullOrEmpty(line.Pid) && line.Pid.IndexOf(text, StringComparison.OrdinalIgnoreCase) > -1) ||
+                             (!string.IsNullOrEmpty(line.Level) && line.Level.IndexOf(text, StringComparison.OrdinalIgnoreCase) > -1);
+        }
+        catch (RegexParseException)
+        {
+          result = false;
+        }
       }
 
       if (!string.IsNullOrEmpty(tenant) && !string.Equals(tenant, All, StringComparison.InvariantCultureIgnoreCase))
@@ -789,5 +802,24 @@ namespace LogViewer
     }
     #endregion
 
+    private void UseRegex_Changed()
+    {
+      int startLength = this.Filter.Text.Length;
+      if (startLength == this.Filter.Text.Length && this.Filter.IsEnabled)
+      {
+        var tenant = this.TenantFilter.SelectedValue as string;
+        var level = this.LevelFilter.SelectedValue as string;
+        this.SetFilter(this.Filter.Text, tenant, level);
+      }
+    }
+    private void UseRegex_Checked(object sender, RoutedEventArgs e)
+    {
+      this.UseRegex_Changed();
+    }
+
+    private void UseRegex_Unchecked(object sender, RoutedEventArgs e)
+    {
+      this.UseRegex_Changed();
+    }
   }
 }
