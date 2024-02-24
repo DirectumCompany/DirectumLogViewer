db3dd44c630355888fafba035c2397a045d94fe1
Муллаханов Константин (Mullakhanov_KS) <Mullakhanov_KS@directum.ru>
2024-02-24 15:58:08 +0400

    Переименование проекта LogViewer в LogViewerWpf

--- LogViewerWpf/Properties/Resources.Designer.cs
+++ LogViewerWpf/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LogViewer.Properties {
+namespace LogViewerWpf.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace LogViewer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LogViewer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LogViewerWpf.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
